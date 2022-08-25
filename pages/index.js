da05bdecfc09381b6bfbf6e1ff5870f6c902e408
Nuel Edeh <nuel@getneat.io>
2022-08-25 15:31:16 -0400

    update apostrophe

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="page-container">
       <Toolbar />
       <div className={styles.main}>
-        <h1> Sandra's News App</h1>
+        <h1> Sandra&apos;s News App</h1>
 
         <h3>🔥 Latest news articles delivered to your homepage.</h3>
       </div>
